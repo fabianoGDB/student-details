9eeed7416beaa03014bb17d3c5c0c1ee06ebb9d2
fabianoGDB <doctorfabi.f@gmail.com>
2025-04-25 21:49:06 -0300

    commit number four: adding the first table html component and comportaments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import {  } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { Student } from '../model/student.type';
+import { FormsModule, FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'students-details-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule, NgbHighlight],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'students-details';
+  filter: FormControl;
+  students: Student[] = [
+	{
+	  name: "Paul Spencer",
+	  city: "Fort Freidafield",
+	  studentImage: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Flag_of_Brazil_%28folding%29.gif",
+	  state: "VT",
+	  country: "Kazakhstan",
+	  physics: 11,
+	  chemistry: 21,
+	  maths: 6,
+	  computer: 5
+	},
+	{
+	  name: "Joseph Bergnaum",
+	  city: "Cartwrightworth",
+	  studentImage: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Flag_of_Brazil_%28folding%29.gif",
+	  state: "VA",
+	  country: "Jordan",
+	  physics: 25,
+	  chemistry: 2,
+	  maths: 4,
+	  computer: 9
+	},
+  ];
+
+  constructor(private fb: FormBuilder){
+	this.filter = this.fb.control("", {nonNullable: true});
+  }
 }
---
+++ src/model/student.type.ts
@@ -0,0 +1,12 @@
+export interface Student {
+    name: string;
+    city: string;
+    studentImage: string;
+    state: string;
+    country: string;
+    physics: number;
+    chemistry: number;
+    maths: number;
+    computer: number;
+}
+  
